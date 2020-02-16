fd34711585f0fb2688c2c78ea218bdb25da80161
mutembeijoe <mutembeijoe@gmail.com>
2020-02-16 15:07:35 +0300

    adds tests for signup view

--- users/tests.py
+++ users/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
-
+from django.urls import reverse, resolve
+from .views import SignUpView
 # Create your tests here.
 
 
@@ -34,3 +35,27 @@ class CustomUserTests(TestCase):
         self.assertTrue(super_user.is_staff)
         self.assertTrue(super_user.is_superuser)
 
+
+class SignUpPageTest(TestCase):
+    def setUp(self) -> None:
+        url = reverse('signup')
+        self.response = self.client.get(url)
+
+    def test_signup_uses_template(self):
+        self.assertTemplateUsed(self.response, 'registration/signup.html')
+
+    def test_signup_response_status(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_signup_url_resolves_SignUpView(self):
+        view = resolve(reverse('signup'))
+        self.assertEqual(
+            view.func.__name__,
+            SignUpView.as_view().__name__
+        )
+
+    def test_signup_page_contains(self):
+        self.assertContains(self.response,'SignUp')
+
+    def test_signup_page_not_contains(self):
+        self.assertNotContains(self.response,'LogIn')
