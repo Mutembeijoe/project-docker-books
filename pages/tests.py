d297fb402809847ffd6366cbbddfddc7f8b81342
mutembeijoe <mutembeijoe@gmail.com>
2020-02-16 19:11:33 +0300

    adds tests for about page :heavy_check_mark:

--- pages/tests.py
+++ pages/tests.py
@@ -1,6 +1,6 @@
 from django.test import SimpleTestCase
 from django.urls import reverse, resolve
-from .views import HomePageView
+from .views import HomePageView, AboutPageView
 
 
 
@@ -26,3 +26,25 @@ class HomePageTests(SimpleTestCase):
 
     def test_homepage_uses_template(self):
         self.assertTemplateUsed(self.response, 'home.html')
+
+
+class AboutPageTests(SimpleTestCase):
+    def setUp(self):
+        url = reverse('about')
+        self.response = self.client.get(url)
+
+    def test_template_used(self):
+        self.assertTemplateUsed(self.response, 'about.html')
+
+    def test_about_page_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_about_page_contains_html(self):
+        self.assertContains(self.response, 'About Page')
+
+    def test_about_page_does_not_contain_html(self):
+        self.assertNotContains(self.response, 'What page is this?')
+
+    def test_about_page_resolves_AboutPageView(self):
+        view = resolve(reverse('about'))
+        self.assertEqual(view.func.__name__, AboutPageView.as_view().__name__)
