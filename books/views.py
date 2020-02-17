9b9ac520394b1c5170521e15eaef67c299c1d23d
mutembeijoe <mutembeijoe@gmail.com>
2020-02-17 23:21:34 +0300

    adds a view for book detail

--- books/views.py
+++ books/views.py
@@ -5,5 +5,10 @@ from .models import Book
 
 class BookListView(ListView):
     model = Book
-    template_name = 'book_list.html'
+    template_name = 'books/book_list.html'
     context_object_name = 'book_list'
+
+class BookDetailView(DetailView):
+    model = Book
+    template_name='books/book_detail.html'
+    context_object_name = 'book'
